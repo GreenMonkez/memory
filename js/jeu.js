fd713f5eee7e198213887e03c5c34c97e48111ee
GreenMonkez <herbreteau.gildas@hotmail.fr>
2024-12-05 10:58:22 +0100

    Change jeu en fct du user

--- js/jeu.js
+++ js/jeu.js
@@ -1,27 +1,508 @@
-const cardsArray = [{
-    "name": "pomme",
+const legume = [{
+    "name": "1",
     "img": "assets/memory-legume/1.svg"
 },
 {
-    "name": "banane",
+    "name": "2",
     "img": "assets/memory-legume/2.svg"
 },
 {
-    "name": "chou",
+    "name": "3",
     "img": "assets/memory-legume/3.svg"
 },
 {
-    "name": "cerise",
+    "name": "4",
     "img": "assets/memory-legume/4.svg"
 },
 {
-    "name": "piment",
+    "name": "5",
     "img": "assets/memory-legume/5.svg"
 },
 {
-    "name": "fraise",
+    "name": "6",
     "img": "assets/memory-legume/6.svg"
 }];
+
+const alphabet = [{
+    "name": "1",
+    "img": "assets/alphabet-scrabble/1.png"
+},
+{
+    "name": "2",
+    "img": "assets/alphabet-scrabble/2.png"
+},
+{
+    "name": "3",
+    "img": "assets/alphabet-scrabble/3.png"
+},
+{
+    "name": "4",
+    "img": "assets/alphabet-scrabble/4.png"
+},
+{
+    "name": "5",
+    "img": "assets/alphabet-scrabble/5.png"
+},
+{
+    "name": "6",
+    "img": "assets/alphabet-scrabble/6.png"
+},
+{
+    "name": "7",
+    "img": "assets/alphabet-scrabble/7.png"
+},
+{
+    "name": "8",
+    "img": "assets/alphabet-scrabble/8.png"
+},
+{
+    "name": "9",
+    "img": "assets/alphabet-scrabble/9.png"
+},
+{
+    "name": "10",
+    "img": "assets/alphabet-scrabble/10.png"
+},
+{
+    "name": "11",
+    "img": "assets/alphabet-scrabble/11.png"
+},
+{
+    "name": "12",
+    "img": "assets/alphabet-scrabble/12.png"
+},
+{
+    "name": "13",
+    "img": "assets/alphabet-scrabble/13.png"
+},
+{
+    "name": "14",
+    "img": "assets/alphabet-scrabble/14.png"
+},
+{
+    "name": "15",
+    "img": "assets/alphabet-scrabble/15.png"
+},
+{
+    "name": "16",
+    "img": "assets/alphabet-scrabble/16.png"
+},
+{
+    "name": "17",
+    "img": "assets/alphabet-scrabble/17.png"
+},
+{
+    "name": "18",
+    "img": "assets/alphabet-scrabble/18.png"
+},
+{
+    "name": "19",
+    "img": "assets/alphabet-scrabble/19.png"
+},
+{
+    "name": "20",
+    "img": "assets/alphabet-scrabble/20.png"
+},
+{
+    "name": "21",
+    "img": "assets/alphabet-scrabble/21.png"
+},
+{
+    "name": "22",
+    "img": "assets/alphabet-scrabble/22.png"
+},
+{
+    "name": "23",
+    "img": "assets/alphabet-scrabble/23.png"
+},
+{
+    "name": "24",
+    "img": "assets/alphabet-scrabble/24.png"
+},
+{
+    "name": "25",
+    "img": "assets/alphabet-scrabble/25.png"
+},
+{
+    "name": "26",
+    "img": "assets/alphabet-scrabble/26.png"
+}];
+
+const animaux = [{
+    "name": "1",
+    "img": "assets/animaux/1.webp"
+},
+{
+    "name": "2",
+    "img": "assets/animaux/2.webp"
+},
+{
+    "name": "3",
+    "img": "assets/animaux/3.webp"
+},
+{
+    "name": "4",
+    "img": "assets/animaux/4.webp"
+},
+{
+    "name": "5",
+    "img": "assets/animaux/5.webp"
+},
+{
+    "name": "6",
+    "img": "assets/animaux/6.webp"
+},
+{
+    "name": "7",
+    "img": "assets/animaux/7.webp"
+},
+{
+    "name": "8",
+    "img": "assets/animaux/8.webp"
+},
+{
+    "name": "9",
+    "img": "assets/animaux/9.webp"
+},
+{
+    "name": "10",
+    "img": "assets/animaux/10.webp"
+},
+{
+    "name": "11",
+    "img": "assets/animaux/11.webp"
+},
+{
+    "name": "12",
+    "img": "assets/animaux/12.webp"
+},
+{
+    "name": "13",
+    "img": "assets/animaux/13.webp"
+},
+{
+    "name": "14",
+    "img": "assets/animaux/14.webp"
+},
+{
+    "name": "15",
+    "img": "assets/animaux/15.webp"
+},
+{
+    "name": "17",
+    "img": "assets/animaux/17.webp"
+},
+{
+    "name": "18",
+    "img": "assets/animaux/18.webp"
+},
+{
+    "name": "19",
+    "img": "assets/animaux/19.webp"
+},
+{
+    "name": "20",
+    "img": "assets/animaux/20.webp"
+},
+{
+    "name": "21",
+    "img": "assets/animaux/21.webp"
+},
+{
+    "name": "22",
+    "img": "assets/animaux/22.webp"
+},
+{
+    "name": "23",
+    "img": "assets/animaux/23.webp"
+},
+{
+    "name": "24",
+    "img": "assets/animaux/24.webp"
+},
+{
+    "name": "25",
+    "img": "assets/animaux/25.webp"
+},
+{
+    "name": "26",
+    "img": "assets/animaux/26.webp"
+},
+{
+    "name": "27",
+    "img": "assets/animaux/27.webp"
+},
+{
+    "name": "28",
+    "img": "assets/animaux/28.webp"
+}];
+
+const animauxAnimes = [{
+    "name": "1",
+    "img": "assets/animauxAnimes/1.webp"
+},
+{
+    "name": "2",
+    "img": "assets/animauxAnimes/2.webp"
+},
+{
+    "name": "3",
+    "img": "assets/animauxAnimes/3.webp"
+},
+{
+    "name": "4",
+    "img": "assets/animauxAnimes/4.webp"
+},
+{
+    "name": "5",
+    "img": "assets/animauxAnimes/5.webp"
+},
+{
+    "name": "6",
+    "img": "assets/animauxAnimes/6.webp"
+},
+{
+    "name": "7",
+    "img": "assets/animauxAnimes/7.webp"
+},
+{
+    "name": "8",
+    "img": "assets/animauxAnimes/8.webp"
+}];
+
+const animauxDomestiques = [{
+    "name": "1",
+    "img": "assets/animauxdomestiques/1.jpg"
+},
+{
+    "name": "2",
+    "img": "assets/animauxdomestiques/2.jpg"
+},
+{
+    "name": "3",
+    "img": "assets/animauxdomestiques/3.jpg"
+},
+{
+    "name": "4",
+    "img": "assets/animauxdomestiques/4.jpg"
+},
+{
+    "name": "5",
+    "img": "assets/animauxdomestiques/5.jpg"
+},
+{
+    "name": "6",
+    "img": "assets/animauxdomestiques/6.jpg"
+},
+{
+    "name": "7",
+    "img": "assets/animauxdomestiques/7.jpg"
+},
+{
+    "name": "8",
+    "img": "assets/animauxdomestiques/8.jpg"
+},
+{
+    "name": "9",
+    "img": "assets/animauxdomestiques/9.jpg"
+},
+{
+    "name": "10",
+    "img": "assets/animauxdomestiques/10.jpg"
+}];
+
+const chiens = [{
+    "name": "1",
+    "img": "assets/chiens/1.webp"
+},
+{
+    "name": "2",
+    "img": "assets/chiens/2.webp"
+},
+{
+    "name": "3",
+    "img": "assets/chiens/3.webp"
+},
+{
+    "name": "4",
+    "img": "assets/chiens/4.webp"
+},
+{
+    "name": "5",
+    "img": "assets/chiens/5.webp"
+},
+{
+    "name": "6",
+    "img": "assets/chiens/6.webp"
+},
+{
+    "name": "7",
+    "img": "assets/chiens/7.webp"
+},
+{
+    "name": "8",
+    "img": "assets/chiens/8.webp"
+},
+{
+    "name": "9",
+    "img": "assets/chiens/9.webp"
+},
+{
+    "name": "10",
+    "img": "assets/chiens/10.webp"
+},
+{
+    "name": "11",
+    "img": "assets/chiens/11.webp"
+},
+{
+    "name": "12",
+    "img": "assets/chiens/12.webp"
+},
+{
+    "name": "13",
+    "img": "assets/chiens/13.webp"
+},
+{
+    "name": "14",
+    "img": "assets/chiens/14.webp"
+},
+{
+    "name": "15",
+    "img": "assets/chiens/15.webp"
+},
+{
+    "name": "16",
+    "img": "assets/chiens/16.webp"
+},
+{
+    "name": "17",
+    "img": "assets/chiens/17.webp"
+},
+{
+    "name": "18",
+    "img": "assets/chiens/18.webp"
+},
+{
+    "name": "19",
+    "img": "assets/chiens/19.webp"
+},
+{
+    "name": "20",
+    "img": "assets/chiens/20.webp"
+},
+{
+    "name": "21",
+    "img": "assets/chiens/21.webp"
+},
+{
+    "name": "22",
+    "img": "assets/chiens/22.webp"
+},
+{
+    "name": "23",
+    "img": "assets/chiens/23.webp"
+}];
+
+const dinosaures = [{
+    "name": "1",
+    "img": "assets/dinosaures/1.jpg"
+},
+{
+    "name": "2",
+    "img": "assets/dinosaures/2.jpg"
+},
+{
+    "name": "3",
+    "img": "assets/dinosaures/3.jpg"
+},
+{
+    "name": "4",
+    "img": "assets/dinosaures/4.jpg"
+},
+{
+    "name": "5",
+    "img": "assets/dinosaures/5.jpg"
+},
+{
+    "name": "6",
+    "img": "assets/dinosaures/6.jpg"
+},
+{
+    "name": "7",
+    "img": "assets/dinosaures/7.jpg"
+},
+{
+    "name": "8",
+    "img": "assets/dinosaures/8.jpg"
+},
+{
+    "name": "9",
+    "img": "assets/dinosaures/9.jpg"
+},
+{
+    "name": "10",
+    "img": "assets/dinosaures/10.jpg"
+}];
+
+const dinosauresAvecNom = [{
+    "name": "1",
+    "img": "assets/dinosauresAvecNom/1.jpg"
+},
+{
+    "name": "2",
+    "img": "assets/dinosauresAvecNom/2.jpg"
+},
+{
+    "name": "3",
+    "img": "assets/dinosauresAvecNom/3.jpg"
+},
+{
+    "name": "4",
+    "img": "assets/dinosauresAvecNom/4.jpg"
+},
+{
+    "name": "5",
+    "img": "assets/dinosauresAvecNom/5.jpg"
+},
+{
+    "name": "6",
+    "img": "assets/dinosauresAvecNom/6.jpg"
+},
+{
+    "name": "7",
+    "img": "assets/dinosauresAvecNom/7.jpg"
+},
+{
+    "name": "8",
+    "img": "assets/dinosauresAvecNom/8.jpg"
+},
+{
+    "name": "9",
+    "img": "assets/dinosauresAvecNom/9.jpg"
+},
+{
+    "name": "10",
+    "img": "assets/dinosauresAvecNom/10.jpg"
+}];
+
+const users = JSON.parse(localStorage.getItem("userData")) ?? [];
+const userEmail = JSON.parse(sessionStorage.getItem("activeUser")) ?? null;
+const user = users.find(user => user.email === userEmail) ?? [];
+const userChoice = user['choice'] ?? null;
+const userSize = user['size'] ?? null;
+
+let cardsArray;
+
+if (userChoice && userSize) {
+    cardsArray = eval(userChoice);
+    cardsArray = cardsArray.sort(function () {
+        return 0.5 - Math.random();
+    })
+    cardsArray.splice(userSize, 99)
+} else {
+    cardsArray = legume;
+}
 
 let nbPaires = cardsArray.length;
 
